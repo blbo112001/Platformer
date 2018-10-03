f9f47b1764f208a66eeecb5da61f6b2c0cfc9ce2
blbo112001 <attard.lewis@gmail.com>
2018-10-03 14:33:46 +1000

    player movemnet prediciton
    Writing the code for the prediciton of the player's movement

--- PlatFormer/PlatFormer/Collision.cs
+++ PlatFormer/PlatFormer/Collision.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,30 @@ namespace PlatFormer
         {
             // create a copy of the hero that will move to where the hero will be in the next frame.
             Sprite playerPrediction = new Sprite();
-            playerPrediction.position = hero.position
+            playerPrediction.position = hero.position;
+            playerPrediction.width = hero.width;
+            playerPrediction.height = hero.height;
+            playerPrediction.offset = hero.offset;
+            playerPrediction.UpdateHitBox();
+
+            playerPrediction.position += hero.velocity * deltaTime;
+
+            int playerColumn = (int)Math.Round(playerPrediction.position.X / game.tileHeight);
+            int playerRow = (int)Math.Round(playerPrediction.position.Y / game.tileHeight);
+
+            Vector2 playerTile = new Vector2(playerColumn, playerRow);
+
+            Vector2 leftTile = new Vector2(playerTile.X - 1, playerTile.Y);
+            Vector2 rightTile = new Vector2(playerTile.X + 1, playerTile.Y);
+            Vector2 topTile = new Vector2(playerTile.X, playerTile.Y - 1);
+            Vector2 bottomTile = new Vector2(playerTile.X, playerTile.Y + 1);
+
+            Vector2 bottomLeftTile = new Vector2(playerTile.X - 1, playerTile.Y + 1);
+            Vector2 bottomRightTile = new Vector2(playerTile.X + 1, playerTile.Y + 1);
+            Vector2 topLeftTile = new Vector2(playerTile.X - 1, playerTile.Y - 1);
+            Vector2 topRightTile = new Vector2(playerTile.X + 1, playerTile.Y - 1);
+
+            return hero;
         }
     }
 }
